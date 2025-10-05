a4381a4410355b758c5137e3881991bc0823c616
BaudilliardAlt <230780735+BaudrillardAlt@users.noreply.github.com>
2025-10-05 21:52:43 +0200

    2025-10-05_21-52

--- architect/later.sh
+++ architect/later.sh
@@ -4,3 +4,6 @@ ya pkg install
 ./install_packages.py --later
 NVIM_LISTEN_ADDRESS= paru -S neovim-git
 nvim --headless "+Lazy! sync" +qa
+
+sudo pacman -Rns linux-firmware linux-zen
+sudo pacman -S linux-firmware-amdgpu linux-firmware-radeon linux-firmware-whence linux-firmware-realtek linux-firmware-other --needed
