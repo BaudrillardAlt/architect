8464952516170020991b807272cdff6f3bf51061
BaudilliardAlt <230780735+BaudrillardAlt@users.noreply.github.com>
2025-10-08 22:28:12 +0200

    2025-10-08T22:28:10+02:00

--- architect/later.sh
+++ architect/later.sh
@@ -7,4 +7,4 @@ nvim --headless "+Lazy! sync" +qa
 rustup default nightly
 cargo install nirius
 sudo pacman -Rns linux-firmware linux-zen
-sudo pacman -S linux-firmware-amdgpu linux-firmware-radeon linux-firmware-whence linux-firmware-realtek linux-firmware-other --needed
+sudo pacman -S linux-firmware-amdgpu linux-firmware-radeon linux-firmware-whence linux-firmware-realtek linux-firmware-other linux-firmware-mediatek --needed
