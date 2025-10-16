5f5739ebc45ddfb1353317ad5b653273ad5d342a
BaudilliardAlt <230780735+BaudrillardAlt@users.noreply.github.com>
2025-10-16 23:59:38 +0200

    Thu Oct 16 23:59:37 CEST 2025

--- architect/later.sh
+++ architect/later.sh
@@ -1,12 +1,10 @@
-firefox accounts.firefox.com
-firefox gmail.com
-firefox https://account.proton.me/mail
+firefox-developer-edition accounts.firefox.com
+firefox-developer-edition gmail.com
+firefox-developer-edition https://account.proton.me/mail
 bat cache --build
 ya pkg install
 python ~/architect/architect/install_packages.py --later
 NVIM_LISTEN_ADDRESS= paru -S neovim-git
 nvim --headless "+Lazy! sync" +qa
-rustup default nightly
-cargo install nirius
 sudo pacman -Rns linux-firmware linux-zen
 sudo pacman -S linux-firmware-amdgpu linux-firmware-radeon linux-firmware-whence linux-firmware-realtek linux-firmware-other linux-firmware-mediatek --needed
