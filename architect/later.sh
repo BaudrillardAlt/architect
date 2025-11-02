e1ccb2dbc2c8c06f971aea8d9359884afbd0ed7b
BaudilliardAlt <230780735+BaudrillardAlt@users.noreply.github.com>
2025-11-02 02:05:00 +0100

    Sun Nov 2 02:04:59 CET 2025

--- architect/later.sh
+++ architect/later.sh
@@ -8,3 +8,4 @@ NVIM_LISTEN_ADDRESS= paru -S neovim-git
 nvim --headless "+Lazy! sync" +qa
 sudo pacman -Rns linux-firmware linux-zen
 sudo pacman -S linux-firmware-amdgpu linux-firmware-radeon linux-firmware-whence linux-firmware-realtek linux-firmware-other linux-firmware-mediatek --needed
+dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"'
