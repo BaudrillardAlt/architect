3d5a16337c83f3a741d607f3e2086d251d794a6a
BaudilliardAlt <230780735+BaudrillardAlt@users.noreply.github.com>
2025-12-31 23:52:21 +0100

    Wed Dec 31 23:52:21 CET 2025

--- architect/later.sh
+++ architect/later.sh
@@ -5,6 +5,7 @@ bat cache --build
 ya pkg install
 python ~/architect/architect/install_packages.py --later
 NVIM_LISTEN_ADDRESS= paru -S neovim-git
+git clone git@github.com:BaudrillardAlt/nvim-config.git ~/.config/nvim
 nvim --headless "+Lazy! sync" +qa
 sudo pacman -Rns linux-firmware linux-zen
 sudo pacman -S linux-firmware-amdgpu linux-firmware-radeon linux-firmware-whence linux-firmware-realtek linux-firmware-other linux-firmware-mediatek --needed
